6725e2f5121a415755979fafff3a2e66d2e3e5f0
jggarcon <40122723+nocrag@users.noreply.github.com>
2025-06-24 10:03:41 -0300

    created initial spring project

---
+++ backend/src/main/java/com/example/react_spring_products/ReactSpringProductsApplication.java
@@ -0,0 +1,13 @@
+package com.example.react_spring_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringProductsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/react_spring_products/ReactSpringProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.react_spring_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
