bdfc901ab8c9f07df01d98d3daec2189c0150d32
jggarcon <40122723+nocrag@users.noreply.github.com>
2025-07-01 00:05:23 -0300

    created initial brand entity

--- backend/src/main/java/com/example/react_spring_products/entities/Brand.java
+++ backend/src/main/java/com/example/react_spring_products/entities/Brand.java
@@ -1,4 +1,15 @@
 package com.example.react_spring_products.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+@Entity
 public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title is required and cannot be empty.")
+    private String title;
+
 }
